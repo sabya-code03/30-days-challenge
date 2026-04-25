a90ece3f32fa3b5fd6639f799738ee9c3da2d3ca
Sabyasachi Ghosh <sabyasachighosh020803@gmail.com>
2026-04-25 15:48:48 +0530

    Implement binary tree insertion and level order traversal

---
+++ day11.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>	
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+void levelOrder(Node * root) {
+        // If the tree is empty
+        if (root == NULL) {
+            return;
+        }
+        // Create a queue to hold pointers to our nodes.
+        queue<Node*> q;
+        q.push(root);
+    
+        while (!q.empty()) {
+            Node* current = q.front();
+            q.pop();
+            cout << current->data << " ";
+            if (current->left != NULL) {
+                q.push(current->left);
+            }
+            if (current->right != NULL) {
+                q.push(current->right);
+            }
+        }
+    }
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+	myTree.levelOrder(root);
+    return 0;
+}
